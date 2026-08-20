5e3bb53f745ca574392891e4f3c75c1b29186fd5
gauwakwe28-cell <gauwakwe28@gmail.com>
2026-08-20 22:11:30 +0100

    Update wallet_analysis.py

--- wallet_analysis.py
+++ wallet_analysis.py
@@ -182,4 +182,4 @@ def home():
 init_db()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
